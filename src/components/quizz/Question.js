f8cd1a8c8876da059a01d9713743ca28feadd59d
EidanU <71008360+EidanU@users.noreply.github.com>
2021-03-20 15:57:02 +0100

    fix score

--- src/components/quizz/Question.js
+++ src/components/quizz/Question.js
@@ -11,30 +11,22 @@ const Question = ({ data }) => {
     const [nbQuestion, setNbQuestion] = useState(0);
     const [finish, setFinish] = useState(false);
 
-    //setQuestionData
     useEffect(()=>{
         document.title = `Questions numéro ${nbQuestion +1}.`;
     },)
 
     function HandleAnswer(i){
 
-        //const addclass = document.getElementsByClassName('divAnsw');
 
-        if(questionData[nbQuestion].vraie === questionData[nbQuestion].reponse[i] && nbQuestion <= 9 && score <10){
-            // addclass.className.add("correct")
-            // setTimeout(function(){
-            //     addclass.classList.remove("correct");
-            // },3000);
-            setScore(score + 1)
-        }
-        else{
-            // addclass.classList.add("faux")
-            //     setTimeout(function(){
-            //         addclass.className.remove("faux");
-            //     },3000);
-            setScore(score)
-        }
-        
+    if(questionData[nbQuestion].vraie === questionData[nbQuestion].reponse[i] && nbQuestion <= 9 && score <10 && finish == false){
+  
+        setScore(score + 1)
+    }
+    else{
+  
+        setScore(score)
+    }
+    
         if (nbQuestion < 9){ 
             setNbQuestion(nbQuestion +1);
         }
