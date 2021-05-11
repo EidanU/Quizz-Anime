c1657ae38efb80ef68bcd7cd336d3eb95571d7ae
EidanU <71008360+EidanU@users.noreply.github.com>
2021-05-11 23:22:35 +0200

    another one

--- src/Route/Router.js
+++ src/Route/Router.js
@@ -10,27 +10,27 @@ import Resultats from '../components/quizz/Resultats';
 
 //data={data[0].snk}
 const Routeur = () => {
-    const myUrl = "eidanu.github.io/Quizz-Anime";
+    const myUrl = "/Quizz-Anime";
     return (
         <>
             <Router>
                 <Switch>
-                    <Route exact path={myUrl} component={Landing} />
-                    <Route exact path={myUrl + '/quizz'} component={Quizz} />
+                    <Route path={myUrl} component={Landing} />
+                    <Route path={myUrl + '/quizz'} component={Quizz} />
 
-                    <Route exact path={myUrl + "/question/1"}>
+                    <Route path={myUrl + "/question/1"}>
                         <Question data={data[0].snk} />
                     </Route>
 
-                    <Route exact path={myUrl + "/question/2"}>
+                    <Route path={myUrl + "/question/2"}>
                         <Question data={data[1].kny} />
                     </Route>
 
-                    <Route exact path={myUrl + "/question/3"}>
+                    <Route path={myUrl + "/question/3"}>
                         <Question data={data[2].dbz} />
                     </Route>
 
-                    <Route exact path={myUrl + "/question/4"}>
+                    <Route path={myUrl + "/question/4"}>
                         <Question data={data[3].naruto} />
                     </Route>
 
